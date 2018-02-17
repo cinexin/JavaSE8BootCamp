44faaf0d9a0925345c45519a154060ebe1a70bf0
cinexin <migueguillamon@gmail.com>
2018-02-17 12:36:45 +0000

    Intro to classes: Car class

---
+++ ClassesIntro/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    String color;
+	String type;
+
+
+	void start() {
+        System.out.println ("Get Started!");
+	}
+
+    void printDescription() {
+        System.out.println ("This is a " + color + " " + type);
+    }
+
+}
