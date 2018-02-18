3559e5f3b06200e9f393eb09f790386f8d734a1f
cinexin <migueguillamon@gmail.com>
2018-02-18 13:57:22 +0100

    Code refactor: Use of this() for extending Car() constructor functionality (Avoiding the DRY 'assignSerialNumber()' method calls)

--- StaticScopeGC/Car.java
+++ StaticScopeGC/Car.java
@@ -8,14 +8,22 @@ public class Car {
     */
     static int carCount;
 
-    {
-        color = "Red";
-        type = "Renault";
+    /* The code in this block will only be executed once: when class is loaded */
+    static {
+        resetCarCount();
     }
 
-    /* static methods don't have access to static vars or methods */
+    /* static methods don't have access to static vars or methods
+        static methods are used to:
+        - for accessing class variable data
+        - provide functionality without having to instatiate an object
+        */
     static void resetCarCount() {
             carCount = 0;
+    }
+
+    public static void setCarCount (int count) {
+        carCount = count;
     }
 
     protected void assignASerialNumber() {
@@ -28,9 +36,11 @@ public class Car {
     }
 
     public Car (String color, String type) {
+        this();
+        // assignASerialNumber();
         this.color = color;
         this.type = type;
-        assignASerialNumber();
+
     }
 
 	void start() {
@@ -38,24 +48,33 @@ public class Car {
 	}
 
     void printDescription() {
-        System.out.println ("This is a " + color + " " + type + ". S / N: " + serialNumber);
+        System.out.println ("This is a " + this.color + " " + this.type + ". S / N: " + this.serialNumber);
     }
 
     public String getDescription() {
         String description = "This is a " + color + " " + type;
         return description;
     }
+
+    void customize(String color, String type) {
+        this.color = color;
+        this.type = type + " " + this.type;
+    }
+
+
     public static void main (String[] args) {
         Car.resetCarCount();
 
         Car fordKaPlus = new Car ("Marine Blue", "Ford K+");
-        Car renaultClio = new Car ("Renault Clio", "Red");
-        Car fordFocus = new Car ("Ford Focus", "Metal grey");
+        Car renaultClio = new Car ("Red","Renault Clio" );
+        Car fordFocus = new Car ("Metal grey", "Ford Focus");
 
         fordKaPlus.printDescription();
         fordKaPlus.start();
 
         renaultClio.printDescription();
+        fordFocus.printDescription();
+        fordFocus.customize ("blue", "convertible");
         fordFocus.printDescription();
     }
 }
