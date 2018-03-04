417b1e1f28fb42fae2a34cbf6b8a0e5b7518c697
cinexin <migueguillamon@gmail.com>
2018-03-04 11:37:33 +0100

    Add PassByExperiment testing class to test passing by value / reference concepts

---
+++ AcmeOrderSystem/src/com/acme/testing/PassByExperiment.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.acme.testing;
+
+import com.acme.utils.MyDate;
+
+/**
+ * @author migui
+ * Lab 7: Parameter passing
+ *
+ */
+public class PassByExperiment {
+
+	public static void passObject(MyDate d) 
+	{
+		d.year = 2009;
+	}
+	
+	public static void passPrimitive (int i) 
+	{
+		i = 2010;
+	}
+	
+	public static void passString (String s) {
+		int yearSlash = s.lastIndexOf('/');
+		s = s.substring(0, yearSlash + 1);
+		s += "2012";
+		System.out.println("New date string: " + s);
+	}
+	
+	
+	/** main method (entry point)
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Passing regular Objects...
+		MyDate date = new MyDate(1,20,2008);
+		System.out.println("Before passing an object " + date);
+		passObject(date);
+		System.out.println("After passing an object " + date);
+		
+		// Passing primitives...
+		System.out.println("Before passing a primitive " + date.year);
+		passPrimitive(date.year);
+		System.out.println("After passing a primitive " + date.year);
+
+		// Passing Strings...
+		String x = date.toString();
+		System.out.println("Before passing a String " + x);
+		passString(x);
+		System.out.println("After passing a String " + x);
+	}
+
+}
