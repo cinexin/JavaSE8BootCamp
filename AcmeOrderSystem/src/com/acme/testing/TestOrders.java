e524842dd344ebffed1a8c73f3253232ac6ca240
cinexin <migueguillamon@gmail.com>
2018-03-17 11:42:52 +0100

    Modify Lambda function setRushable() to set to true if age of Order > 1 month (using Java 8 DateTime utilites)

--- AcmeOrderSystem/src/com/acme/testing/TestOrders.java
+++ AcmeOrderSystem/src/com/acme/testing/TestOrders.java
@@ -1,4 +1,7 @@
 package com.acme.testing;
+import java.time.LocalDate;
+import java.time.Period;
+
 import com.acme.domain.Good;
 import com.acme.domain.Good.UnitOfMeasureType;
 import com.acme.domain.Order;
@@ -12,7 +15,7 @@ public class TestOrders {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MyDate date1 = new MyDate(1,20,2008);
+		MyDate date1 = new MyDate(12,03,2018);
 		Solid s1 = new Solid("Acme Anvil", 1668, 0.3,    UnitOfMeasureType.CUBIC_METER, false, 500, 0.25, 0.3); 
 		Order anvil = new Order(date1, 2000.00, "Anvil",s1, 10);
 
@@ -45,16 +48,33 @@ public class TestOrders {
 		System.out.println("The volume of the anvil is:  " + ((Good) anvil.getProduct()).volume()); 
 		System.out.println("The length of the anvil is:  " + ((Solid)anvil.getProduct()).getLength()); 
 		
-		MyDate date4 = new MyDate(4,10,2008); 
+		MyDate date4 = new MyDate(12,03,2018); 
 		Service s3 = new Service("Road Runner Eradication", 14, false); 
 		Order birdEradication = new Order(date4, 20000, "Daffy Duck", s3, 1); 
 		System.out.println("The total bill for: " + birdEradication + " is "     + birdEradication.computeTotal()); 
 		
-		Order.setRushable((orderDate,orderAmount) -> {return (orderAmount > 1500);});
+		Order.setRushable((orderDate,orderAmount) -> 
+			{
+				LocalDate theOrderDate = LocalDate.of(orderDate.getYear(), orderDate.getMonth(), orderDate.getDay());
+				LocalDate todayMinus1Month = LocalDate.now().minusMonths(1);
+				if (theOrderDate.isBefore(todayMinus1Month)) {
+					return true;
+				} else {
+					return false;
+				}
+				
+			});
 		
 		
-		System.out.println("Anvil isPriorityOrder: " + anvil.isPriorityOrder());
-		System.out.println("Balloons isPriorityOrder: " + balloons.isPriorityOrder());
+		/* System.out.println("Anvil isPriorityOrder: " + anvil.isPriorityOrder());
+		System.out.println("Balloons isPriorityOrder: " + balloons.isPriorityOrder()); */
+		
+		MyDate hammerDate = new MyDate( 3, 1, 2018 );
+		Solid hammerType = new Solid( "Acme Hammer", 281, 0.3,
+		UnitOfMeasureType.CUBIC_METER, false, 100, 0.25, 0.3 );
+		Order hammer = new Order( hammerDate, 10.00, "Wile E Coyote",hammerType, 10 );
+		System.out.println("Anvil isPriorityOrder: " +anvil.isPriorityOrder());
+		System.out.println("Hammer isPriorityOrder: " + hammer.isPriorityOrder());
 	}
 
 }
