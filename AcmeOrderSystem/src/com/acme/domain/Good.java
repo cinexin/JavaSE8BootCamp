9f3edeb06ca33b266038d83b3428a2e51803e968
cinexin <migueguillamon@gmail.com>
2018-03-18 11:47:00 +0100

    Change catalog member type (Collection) to be a Set instead of a List

--- AcmeOrderSystem/src/com/acme/domain/Good.java
+++ AcmeOrderSystem/src/com/acme/domain/Good.java
@@ -23,7 +23,7 @@ public abstract class Good implements Product{
 	private UnitOfMeasureType unitOfMeasure; 
 	private boolean flammable = true;
 	private double weightPerUofM;
-	private static List catalog;
+	private static Set catalog;
 	
 	static {
 		Liquid glue = new Liquid("Acme Glue", 2334, 4, UnitOfMeasureType.LITER,false, 15, 6);
@@ -35,7 +35,7 @@ public abstract class Good implements Product{
 		Liquid nitro = new Liquid("Acme Nitroglycerin", 4289, 1.0,UnitOfMeasureType.CUBIC_METER, true, 1.5, 0.25);
 		Liquid oil = new Liquid("Acme Oil", 4275, 1.0,UnitOfMeasureType.CUBIC_METER, true, 1.5, 0.25);
 		
-		catalog = new ArrayList<>();
+		catalog = new HashSet();
 		catalog.add(anvil);
 		catalog.add(safe);
 		catalog.add(balloon);
@@ -49,7 +49,7 @@ public abstract class Good implements Product{
 	/**
 	 * @return the catalog
 	 */
-	public static List getCatalog() {
+	public static Set getCatalog() {
 		return catalog;
 	}
 	/**
